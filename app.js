77786f7b2d4b499718cc9f490d369b147cec7c88
HoneyMakhija <55359389+HoneyMakhija@users.noreply.github.com>
2023-11-25 16:02:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ const productsRouter = require('./routes/products');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API with Swagger',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'], // files containing annotations as above
+};
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+
 // middleware
 app.use(express.json());
 
